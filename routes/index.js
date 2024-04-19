72fb5cf126e9e09493c4b8029f56a7f29c3ea402
Peter Adeyemo <adeyemo@tm30.net>
2024-04-19 11:16:44 +0100

    Single use of every code

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 const swal = require("sweetalert");
+const usedCodes = [];
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { base: "Express" });
@@ -14,9 +15,12 @@ router.post("/submit", async (req, res) => {
   fileInJson = JSON.parse(fileInJson);
 
   const user = fileInJson.find((el) => el.code === `ET-${seat_number}`);
-  console.log(user);
-  if (user) return res.render("index", user);
-  else return res.render("index", { error: "Not found" });
+  if (user) {
+    if(usedCodes[seat_number])
+      return res.render("index", {error: "Oops... User Code used already..."})
+    usedCodes[seat_number] = true;
+    return res.render("index", user);
+  } else return res.render("index", { error: "Oops! Code does not exist." });
 });
 
 module.exports = router;
